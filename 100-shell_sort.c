1808c048c8a4e96900426fd36f4eb84e88d2d017
lakelexy <alao8380@yahoo.com>
2023-01-24 15:36:39 +0000

    100-shell_sort.c

--- 100-shell_sort.c
+++ 100-shell_sort.c
@@ -1,63 +1,48 @@
 #include "sort.h"
-/**
- * _swap - swap two numbers.
- * @a: integer
- * @b: integer
- **/
 
-void _swap(int *a, int *b)
+/**
+ * get_max_gap - gets the a largest Knuth Sequence gap for this size
+ * @size: the size of the array
+ *
+ * Return: the gap size
+ */
+size_t get_max_gap(size_t size)
 {
-	int tmp;
+	size_t n;
 
-	tmp = *a;
-	*a = *b;
-	*b = tmp;
+	n = 1;
+	while (n < size)
+		n = n * 3 + 1;
+	return ((n - 1) / 3);
 }
-/**
- * backward_insertion -swap two nodes right left position
- * @array: array
- * @gap: gap
- * @act: actual position in the array
- **/
-void backward_insertion(int *array, int gap, int act)
-{
-	int i;
 
-	for (i = act - gap; i >= 0; i -= gap, act -= gap)
-	{
-		if (array[i] > array[act])
-			_swap(&array[i], &array[act]);							else
-		else
-			break;
-	}
-}
 /**
- * shell_sort -Sort an array using shell_sort algorithm
- * @array: array
- * @size: size
- **/
+ * shell_sort - shell_sort
+ * @array: the integer array to sort
+ * @size: the size of the array
+ *
+ * Return: void
+ */
 void shell_sort(int *array, size_t size)
 {
-	unsigned int gap = 1, i, j;
+	size_t gap, i, j;
+	int temp;
 
-	if (array == NULL)
+	if (!array || !size)
 		return;
-	if (size < 2)
-		return;
-	while (gap < size / 3)
-		gap = gap * 3 + 1;
 
-	while (gap > 0)
+
+	for (gap = get_max_gap(size); gap; gap = (gap - 1) / 3)
 	{
-		for (i = 0, j = gap; j < size; i++, j++)
-		{
-			if (array[i] > array[j])
+		for (i = gap; i < size; i++)
+		{											{
+			temp = array[i];
+			for (j = i; j > gap - 1 && array[j - gap] > temp; j -= gap)
 			{
-				_swap(&array[i], &array[j]);
-				backward_insertion(array, gap, i);
+				array[j] = array[j - gap];
 			}
+			array[j] = temp;
 		}
 		print_array(array, size);
-		gap /= 3;
 	}
 }
